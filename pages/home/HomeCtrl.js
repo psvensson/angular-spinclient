cc138c9af7de42f7e2753db13a8f083e80b56246
Peter Svensson <psvenson@gmail.com>
2015-03-29 15:16:42 +0200

    foo

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'psvenssonSpinclient', function($scope, psvenssonSpinclient) {
+	//TODO - put any directive code here
+}]);
